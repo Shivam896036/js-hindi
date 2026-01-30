96dfd3e12b70b47512d6cee10278dde55348eaa0
Shivam896036 <shivam445yadav@gmail.com>
2026-01-30 13:48:12 +0000

    third commit

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict";// treat all JS code as newwr version 
+//alert(3+3)// we are using nodejs , not browser
+console.log(3
+    +3
+)//code readability should be high
+console.log("Shivam Yadav")
+
+let name="Shivam Yadav"
+let age=22
+let isLoggedIn=false
+
+//number=> 2 to power 53
+//bigint
+//String
+//boolean=> true/false
+//null=> standalone value
+//undefined
+//Symbol=> unique
+
+//object
+console.log(typeof undefined);
+console.log(typeof null);
