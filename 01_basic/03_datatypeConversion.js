9f41ab4d9c1707358044b40ccaa1b1616384f193
Shivam896036 <shivam445yadav@gmail.com>
2026-01-30 15:14:58 +0000

    Data Type Conversion

---
+++ 01_basic/03_datatypeConversion.js
@@ -0,0 +1,20 @@
+//let score="Shivam"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+//let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=> 33
+//"33abs"=> NaN (not a number)
+//true=> 1; false=>0
+
+// let isLoggedIn="Shivam"
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber =33
+let StringNuber=String(someNumber)
+console.log(StringNuber);
+console.log(typeof StringNuber);
